4a3f00aaf1486ceee582bc9d20c9ad2627d02653
kbgoda <kbgoda@gmail.com>
2020-02-12 10:21:42 +1100

    Tree displays on html

--- StormWatchers/StormWatchers.py
+++ StormWatchers/StormWatchers.py
@@ -1,16 +1,45 @@
-from flask import Flask, render_template
+from flask import *
+# Flask, render_template, json, current_app as app
+import sys
+
+sys.path.insert(0, '/the/folder/path/python_files')
+
+from python_files.node import *
+from python_files.pass_forms_data import *
+
+o1 = OperatorNode('OR')
+o2 = OperatorNode('AND')
+o2.add_child(ConditionalNode('cpu', '>', '60'))
+o2.add_child(ConditionalNode('ram', '>=', '80'))
+o2.add_child(ConditionalNode('heat', '>=', '100'))
+
+o1.add_child(ConditionalNode('cpu', '<', '2'))
+o1.add_child(o2)
+
+print(o1)
 
 app = Flask(__name__)
 
 # index
 @app.route('/')
-def render_static_1():
+def render_static_index():
     return render_template('index.html', num=10)
 
 # another page
-@app.route('/home')
-def render_static_2():
-    return render_template('sample_tree.html', num=5)
+@app.route('/tree')
+def render_static_tree():
+    return render_template('sample_tree.html', o1=o1)
+
+# email page
+@app.route('/email')
+def render_static_email():
+    return render_template('email.html')
+
+# form page
+@app.route('/form', methods=['POST'])
+def render_static_forms():
+    projectpath = request.form['projectFilepath']
+    return render_template('form.html')
 
 if __name__ == '__main__':
     app.run()
