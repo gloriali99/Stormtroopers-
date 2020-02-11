7a8de94326f6b33767ae2acd5f03392cf9f9f666
dstiasny <dstiasny@outlook.com>
2020-02-11 15:22:43 +1100

    Fixed some bugs and example of how nodes are linked

--- node.py
+++ node.py
@@ -22,12 +22,21 @@ class OperatorNode:
     value = None
     children = None
 
-    def OperatorNode(self, value):
+    def __init__(self, value):
         self.value = value
         self.children = []
     
     def add_child(self, child):
         self.children.append(child)
+
+    def __str__(self):
+        ret = str(self.value) + ": "
+        ret += "["
+        for c in self.children:
+            ret += str(c) + ', '
+        ret = ret[:-2]
+        ret += ']'
+        return ret
 
 
 class ConditionalNode:
@@ -35,7 +44,26 @@ class ConditionalNode:
     operator = None
     threshold = None
 
-    def ConditionalNode(self, a, o, t):
+    def __init__(self, a, o, t):
         self.attribute = a
         self.operator = o
         self.threshold = t
+    
+    def __str__(self):
+        ret = str(self.attribute) + ' ' + self.operator + ' ' + self.threshold
+        return ret
+
+# TODO: DELETE
+# EXAMPLE OF  WORKS
+
+o1 = OperatorNode('OR')
+
+o2 = OperatorNode('AND')
+o2.add_child(ConditionalNode('cpu', '>', '60'))
+o2.add_child(ConditionalNode('ram', '>=', '80'))
+o2.add_child(ConditionalNode('heat', '>=', '100'))
+
+o1.add_child(ConditionalNode('cpu', '<', '2'))
+o1.add_child(o2)
+
+print(o1)
