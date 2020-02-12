b0129c2e921d1d3e51bf2b01b00ab1fd0d7d514f
brendantrinh8 <56022976+brendantrinh8@users.noreply.github.com>
2020-02-12 14:20:45 +1100

    counting buttons

--- templates/main.js
+++ templates/main.js
@@ -2,3 +2,26 @@ function loadDashboard() {
 
     console.log('world')
 }
+
+
+// function Windowopen() {
+// document.getElementById("myChart")
+// }
+// 
+
+function plusFunction() {
+    let ruletext = "Rule";
+    let rulecount = 1;
+
+    let button = document.createElement("button");
+    let buttonwrapper = document.createElement("li");
+    let container = document.getElementById("plus");
+    buttonwrapper.appendChild(button);
+    container.appendChild(buttonwrapper);
+
+    button.classList.add("btn");
+    button.classList.add("btn-dark");
+    buttonwrapper.classList.add("nav-item");
+
+    button.innerHTML = "Rule" + String( 1);
+}
