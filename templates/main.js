c1180aaf7625c27ee2e7f85dca756958f4f8602d
brendantrinh8 <56022976+brendantrinh8@users.noreply.github.com>
2020-02-13 14:06:06 +1100

    fix js

--- templates/main.js
+++ templates/main.js
@@ -1,5 +1,84 @@
 function loadDashboard() {
 
+    $('#builder').queryBuilder({
+        plugins: ['bt-tooltip-errors'],
+        filters: [{
+          id: 'name',
+          label: 'Name',
+          type: 'string' },
+        {
+          id: 'category',
+          label: 'Category',
+          type: 'integer',
+          input: 'select',
+          values: {
+            1: 'Books',
+            2: 'Movies',
+            3: 'Music',
+            4: 'Tools',
+            5: 'Goodies',
+            6: 'Clothes' },
+          operators: ['equal', 'not_equal', 'less', 'not_in', 'is_null', 'is_not_null'] },
+        {
+          id: 'in_stock',
+          label: 'In stock',
+          type: 'integer',
+          input: 'radio',
+          values: {
+            1: 'Yes',
+            0: 'No' },
+          operators: ['equal'] },
+        {
+          id: 'price',
+          label: 'Price',
+          type: 'double',
+          validation: {
+            min: 0,
+            step: 0.01 } },
+        {
+          id: 'id',
+          label: 'Identifier',
+          type: 'string',
+          placeholder: '____-____-____',
+          operators: ['equal', 'not_equal'],
+          validation: {
+            format: /^.{4}-.{4}-.{4}$/ } }],
+        rules: rules_data });
+    /****************************************************************
+                                                Triggers and Changers QueryBuilder
+                         *****************************************************************/
+                         $('#btn-get').on('click', function () {
+    var result = $('#builder').queryBuilder('getRules');
+    if (!$.isEmptyObject(result)) {
+      rules_data = result;
+    }
+    result = $('#builder').queryBuilder('getRules');
+    if (!$.isEmptyObject(result)) {
+      var to_return = JSON.stringify(result, null, 2);
+      $.post( "/postmethod", {
+        javascript_data : to_return
+      });
+      
+    } else
+    {
+      console.log("invalid object :");
+    }
+    console.log(result);
+  });
+  $('#btn-reset').on('click', function () {
+  //   $('#builder').queryBuilder('reset');
+  });
+  $('#btn-set').on('click', function () {
+    //$('#builder').queryBuilder('setRules', rules_data);
+    var result = $('#builder').queryBuilder('getRules');
+    if (!$.isEmptyObject(result)) {
+      rules_data = result;
+    }
+  });
+  //When rules changed :
+  $('#builder').on('getRules.queryBuilder.filter', function (e) {
+    //$log.info(e.value);
+  });
     console.log('world')
     console.log(rules_data)
     loadRulesButton()
