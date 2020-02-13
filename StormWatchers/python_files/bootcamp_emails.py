7f5cff9535ac32af311dfd0f0c313e023c242dd1
Anoshawott <54537931+Anoshawott@users.noreply.github.com>
2020-02-13 11:45:18 +1100

    credentials

--- StormWatchers/python_files/bootcamp_emails.py
+++ StormWatchers/python_files/bootcamp_emails.py
@@ -20,20 +20,29 @@ def send_email(to, cc, reply_email, issue_number):
     cc_str = ",".join(cc)
     all_recipients = to + cc
 
-    sender = "stormtrooperlabs2020@gmail.com"
-    username = "AKIA3U3Z4ZXN726UUD7C"
-    password = "BEwq9ALTo30q0DEYu7q7dxa3DCv0YzPIWTs8Vp435GnT"
+    try:
+        f = open("C:\\Users\\anosh\.aws\credentials.json", "r")
+    except OSError:
+        print("Could not open/read credentials file: credentials.json")
+        return
+
+    with f:
+        credentials = json.load(f)
+        username = credentials["aws_access_key_id"]
+        password = credentials["aws_secret_access_key"]
+
+    sender = "stormtrooper2020labz@gmail.com"
     host = "email-smtp.ap-southeast-2.amazonaws.com"
     port = 587  # innovate wifi
 
     message["Subject"] = "Email Alert"
-    message["From"] = "Stormtroopers <stormtrooperlabs2020@gmail.com>"
+    message["From"] = "Stormtroopers <stormtrooper2020labz@gmail.com>"
     message["To"] = to_str
     message["CC"] = cc_str
     message['Reply-to'] = "StormWatch <" + reply_email + ">"
 
     # Access the event in the kvstore. Uses the format of the issues_detected.json schema
-    issues_kv = KVStore(collection="test_files/issues_detected", key='issue-number') # Calls to Darius' kvstore.py
+    issues_kv = KVStore(collection="../test_files/issues_detected", key='issue-number') # Calls to Darius' kvstore.py
     issue = issues_kv.get_one(issue_number)
 
     # Format email content into string for MIMEText input
@@ -110,15 +119,16 @@ def populate_table(alert_chain):
 
     rows = []
 
+
     # decimal places
     for anomaly in alert_chain:
         raw_time = float(anomaly["_time"])
-        day = datetime.fromtimestamp(raw_time).strftime("%-d")
-        month = datetime.fromtimestamp(raw_time).strftime("%b")
-        year = datetime.fromtimestamp(raw_time).strftime("%Y")
-        _time = datetime.fromtimestamp(raw_time).strftime("%X")
+        # day = datetime.fromtimestamp(raw_time).strftime("%d")
+        # month = datetime.fromtimestamp(raw_time).strftime("%b")
+        # year = datetime.fromtimestamp(raw_time).strftime("%Y")
+        # _time = datetime.fromtimestamp(raw_time).strftime("%X")
 
-        date = " ".join([day,month,year])
+        # date = " ".join([day,month,year])
 
         row = """<tr class="table">
                     <td class="table">{date}</td>
@@ -127,14 +137,14 @@ def populate_table(alert_chain):
                     <td class="table">{value}</td>
                     <td class="table">{expected_value}</td>
                     <td class="table">{severity}</td>
-                </tr>""".format(date = date, _time=_time, metric=anomaly["metric"], value=round(anomaly["value"], 4), expected_value=round(anomaly["expected_value"], 4), severity=round(anomaly["severity"], 4))
+                </tr>""".format(date = "date", _time="_time", metric=anomaly["metric"], value=round(anomaly["value"], 4), expected_value=round(anomaly["expected_value"], 4), severity=round(anomaly["severity"], 4))
         rows.append(row)
 
     return "".join(rows)
 
 if __name__ == "__main__":
-    to = ['stormtrooperlabs2020@gmail.com']
-    cc = ['stormtrooperlabs2020@gmail.com']
-    reply_email = 'stormtrooperlabs2020@gmail.com'
+    to = ['stormtrooper2020labz@gmail.com']
+    cc = ['stormtrooper2020labz@gmail.com']
+    reply_email = 'stormtrooper2020labz@gmail.com'
     issue_number = 3
     send_email(to, cc, reply_email, issue_number)
